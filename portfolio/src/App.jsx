142840bf86ad47f28333b6c46683fdb009762329
zaid-kh <z.kh2800@gmail.com>
2023-11-23 21:31:49 +0200

    initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import zaidLogo from "./assets/head.png";
+
+function App() {
+  return (
+    <>
+      <img src={zaidLogo} alt="zaid's head" style={{ width: 100 }} />
+      <h1>Zaid Khamis's Portfolio</h1>
+    </>
+  );
+}
+
+export default App;
